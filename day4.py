1a817644036b2a07e7fbe09049add2dc799d41aa
Advaith Sai <advaith.maddipatla@gmail.com>
2020-04-05 16:08:48 +0400

    day 4 changes

--- day4.py
+++ day4.py
@@ -1,8 +1,5 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         k = len(nums)
         n = nums.count(0)
         nums.extend([0]*n)
